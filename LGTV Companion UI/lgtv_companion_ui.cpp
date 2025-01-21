ae45912b0c7ac5e0e914181136ecea9e97a5fee5
JPersson77 <jorgen.persson@gmail.com>
2025-01-22 00:20:23 +0100

    ARM64 support

--- LGTV Companion UI/lgtv_companion_ui.cpp
+++ LGTV Companion UI/lgtv_companion_ui.cpp
@@ -46,8 +46,7 @@ INSTALLATION, USAGE ETC
 	https://github.com/JPersson77/LGTVCompanion
 
 LICENSE
-	Copyright (c) 2021-2024 Jörgen Persson
-
+	
 	Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation
 	files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy,
 	modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the
@@ -61,7 +60,7 @@ LICENSE
 	ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 COPYRIGHT
-	Copyright (c) 2021-2024 Jörgen Persson
+	Copyright (c) 2021-2025 Jörgen Persson
 */
 
 #include "targetver.h"
